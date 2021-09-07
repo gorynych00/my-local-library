ff701e4b655e49e5ede9d91c860e71026f84368f
Egor Lynov <gorynych00@gmail.com>
2021-09-07 21:25:45 +0300

    Changed ALLOWED_HOSTS in settings.py

--- loclallibrary/settings.py
+++ loclallibrary/settings.py
@@ -27,7 +27,11 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-l25g59c=_$q81@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '0.0.0.0'
+    'the-yogify.herokuapp.com',
+    '127.0.0.1'
+]
 
 
 # Application definition
